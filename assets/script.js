6559d1bdbba26b2d4c4e8594864da3e152e4b2b2
Erika <erikasalcedo@Erikas-MacBook-Pro.local>
2021-01-15 22:31:03 -0800

    added multiple choice functionality and ability to compare value to array

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,45 @@
-//define document elements that will be used (extra fluff from other class examples that i repurposed)
-var timerEl = document.getElementById('countdown');
-var mainEl = document.querySelector('.main');
-var startBtn = document.getElementById('start');
-var endBtn = document.getElementById('end');
-var quizEl = document.querySelector(".welcome");
 
 //create the question object with answers and options
 let dunderQuestions = [
     {
         id: 1,
         question: "1. What is Dwights middle name?",
-        choices: ["Danger", "Kirk","Kurt","Belsnickel" ],
+        choices: ["Danger", "Kirk", "Kurt", "Belsnickel"],
         correct: "Kurt"
     },
     {
         id: 2,
         question: "2. What was Ryan Howard's social app called?",
-        choices:["Meow", "Chirp","Woof", "Bark" ],
+        choices: ["Meow", "Chirp", "Woof", "Bark"],
         correct: "Woof"
     },
     {
         id: 3,
         question: "3. Would I rather be feared or loved?",
-        choices: ["Loved", "Feared","Hated","Both. I want people to be afraid of how much they love me" ],
+        choices: ["Loved", "Feared", "Hated", "Both. I want people to be afraid of how much they love me"],
         correct: "Both. I want people to be afraid of how much they love me"
 
     },
     {
         id: 4,
         question: "4. Michael Scott is...",
-        choices: ["Tina Turner...always", "Superman", "Beyonce...always", "Whitney...always" ],
+        choices: ["Tina Turner...always", "Superman", "Beyonce...always", "Whitney...always"],
         correct: "Beyonce...always"
     }
 
 ];
+
+//define document elements that will be used (extra fluff from other class examples that i repurposed)
+var timerEl = document.getElementById('countdown');
+var mainEl = document.querySelector('.main');
+var startBtn = document.getElementById('start');
+var endBtn = document.getElementById('end');
+var quizEl = document.querySelector(".welcome");
+var admirableEl = document.getElementById('admirableScore');
+var impishEl = document.getElementById('impishScore');
+var impishScore = 0;
+var admirableScore = 0;
+var finished;
 
 
 
@@ -44,127 +50,140 @@ $("#start").on("click", function () {
     //remove little harvey description
     $(".description").hide();
     $(".btn").hide();
-    console.log("buttons should be hidden and questions should appear");
-    //display quiz question text
-    $(".quiz-text").show();
-    $("#button-display").show();
-    console.log("first question appear?");
-    // countdown();
     displayQuestion();
-
-
-
+    countdown();
 
 });
+//check to see if button selection matches correct value for that question[i]
+function check(qIndex, choiceText) {
+    var selectChoice = (choiceText === dunderQuestions[qIndex].correct);
+    console.log(selectChoice)
+    if (selectChoice) {
+        alert("Admirable, friend!")
+        admirableScore++;
+        admirableEl.textContent = admirableScore;
+        timeLeft -= 1;
+    } else {
+        alert("Impish! WRONG")
+        impishScore++;
+        impishEl.textContent = impishScore;
+        console.log(choiceText)
+        timeLeft -= 15;
 
+    }
+}
 
-
-// choice selection
-// $("#button-choice").on("click", ".correctButton", function (select) {
-//     // correctButton.clicked(e); 
-//     var selectedAnswer = $(select.target).attr("data-name");
-//     console.log(select);
-//     console.log($(select.target).attr("data-name"));
-//     quizQuestion.checkAnswer(selectedAnswer);
-// })
-//questions to appear
 function displayQuestion() {
-    var index = 0
+    var qIndex = 0
     quizBox = document.createElement("div");
-    // quizText.setAttribute("class", "quiz-text");
-    quizBox.textContent = dunderQuestions[index].question;
-    
+    quizBox.setAttribute("id", dunderQuestions)
+
+    // quizBox.textContent = dunderQuestions[index].question;
+
     var quizText = document.createElement("h2")
-    quizText.textContent = dunderQuestions[index].question;
+    quizText.textContent = dunderQuestions[qIndex].question;
     console.log(quizText)
 
     var quizAnswers = document.createElement("ul");
     // quizAnswers.setAttribute("class", "quiz-answers");
-    for (var i=0;i<dunderQuestions.length;i++){
+
+    for (var i = 0; i < dunderQuestions[qIndex].choices.length; i++) {
         var list = document.createElement("li");
+
         var buttonEl = document.createElement("button");
-        buttonEl.textContent = dunderQuestions[index].choices[i];
+        buttonEl.setAttribute("answer-content", dunderQuestions[qIndex].choices[i]);
+
+        buttonEl.textContent = dunderQuestions[qIndex].choices[i];
         list.appendChild(buttonEl);
         quizAnswers.appendChild(list);
         console.log(buttonEl);
     }
+
     quizBox.appendChild(quizText);
     quizBox.appendChild(quizAnswers);
     mainEl.replaceChild(quizBox, quizEl)
 
 
-
-
-
-  
+    //assign the selected button to choiceText and compare that to correctSelected function
+    var choiceList = quizAnswers.querySelectorAll("button");
+    quizAnswers.addEventListener("click", function (e) {
+        var el = e.target;
+        if (el.matches("button")) {
+            var choiceText = el.getAttribute("answer-content")
+            console.log(el.getAttribute("answer-content"));
+            check(qIndex, choiceText);
+            qIndex++;
+            if (qIndex === dunderQuestions.length) {
+                quizComplete();
+                clearInterval(timeInterval);
+                return null;
+            }
+            quizText.textContent = dunderQuestions[qIndex].question;
+            for (var j = 0; j < dunderQuestions[qIndex].choices.length; j++) {
+                choiceList[j].textContent = dunderQuestions[qIndex].choices[j];
+                choiceList[j].setAttribute("answer-content", dunderQuestions[qIndex].choices[j]);
+            }
+        }
+    })
 };
 
-// //button choices display
-// function choiceGenerator() {
-//     $("#button-choice").empty();
-//     for (var i = 0; dunderQuestionsObj.dunderQ[i].length; i++) {
-//         $("#button-choice").append("<li>");
-//         var answer = $("<button>");
-//         answer.attr("data-name", dunderQuestionsObj.dunderQ.correct[i]);
-//         answer.text(dunderQuestions.dunderQ.correct[i]);
-//         //display button
-//         $("button-choice").append(answer);
-//         $("button-choice").append("<li>")
 
-//     };
-// };
-
-
-
+function quizComplete() {
+    // if (index === dunderQuestions.length) {
+    finished = document.createElement("div");
+    var message1 = document.createElement("h1");
+    message1.textContent = "You are finished.";
+    var message2 = document.createElement("h3");
+    message2.textContent = "...That's what she said. or he said..."
+    var score = document.createElement("p");
+    score.textContent = "You earned " + admirableScore + " admirable point(s) and " + impishScore + " impish points"
+    finished.appendChild(message1);
+    finished.appendChild(message2);
+    finished.appendChild(score);
+    //replace current quizbox text with the new finished message
+    mainEl.replaceChild(finished, quizBox);
 
 
+}
 
+function countdown() {
+    timeLeft = 100;
+    // time interval using setInterval method for countdown
+    var timeInterval = setInterval(function () {
+        //for loop conditions for when to apply which message
+        if (timeLeft > 1) {
 
+            timerEl.textContent = timeLeft + ' seconds remaining';
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            timerEl.textContent = timeLeft + ' second remaining';
+            timeLeft--;
+        } else {
+            // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+            timerEl.textContent = '';
+            // Use `clearInterval()` to stop the timer
+            clearInterval(timeInterval);
+            quizComplete();
+            // Call the `displayMessage()` function
+            alert("Time is up!");
+        }
 
-// // Timer function that decreases by second
-// function countdown() {
-//     var timeLeft = 10;
-//     // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-//     var timeInterval = setInterval(function () {
-//         if (timeLeft > 1) {
-//             timeLeft--;
-//             timerEl.textContent = timeLeft + " seconds remaining";
-//         } else if (timeLeft === 1) {
-//             timeLeft--;
-//             timerEl.textContent = timeLeft + " second remaining";
-//         }
-//         else {
-//             timerEl.textContent = "";
-//             clearInterval(timeInterval);
-//             displayMessage();
-
-//         }
-//     }, 1000);
-// };
-
-
-
-
-
-// //If the correct answer is selected, then incremement Admirable points by 1
-// function correctSelected(correct) {
+    }, 1000);
+};
 
 
 
-// }
 
 
-// // Displays the message one word at a time
-// function displayMessage() {
-//     var wordCount = 0;
 
-//     // Uses the `setInterval()` method to call a function to be executed every 300 milliseconds
-//     var msgInterval = setInterval(function () {
-//         if (words[wordCount] === undefined) {
-//             clearInterval(msgInterval);
-//         } else {
-//             mainEl.textContent = words[wordCount];
-//             wordCount++;
-//         }
-//     }, 300);
-// }
+
+
+
+
+
+
+
+
+
+
+
